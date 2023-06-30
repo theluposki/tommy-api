d773df38ad637aa9ef65255bdd25eaa50270ab46
theluposki <theluposki@gmail.com>
2023-06-30 17:54:01 -0300

    build http/https

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import https from "node:https"
+import http from "node:http"
+
 import app from './app.js'
 import config from './config.js'
 
 console.log("\nPRODUCTION", config.production, "\n")
 
-const options = config.production === 'true' ? {} : config.certificates
+if(config.production === "true") {
+  const server = http.createServer(app)
 
-const server = https.createServer(options, app)
+  server.listen(config.app.PORT, () => {
+    console.log(`[ APP ] Listening at 🚀http://localhost:${config.app.PORT}`)
+  })
 
-server.listen(config.app.PORT, () => {
-  console.log(`[ APP ] Listening at 🚀${config.app.HOST}:${config.app.PORT}`)
-})
+} else {
+  const options = config.certificates
+  const server = https.createServer(options, app)
+
+  server.listen(config.app.PORT, () => {
+    console.log(`[ APP ] Listening at 🚀${config.app.HOST}:${config.app.PORT}`)
+  })  
+}
+
+
+
+
+
