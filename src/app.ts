95bbdc698e895de8dd9cab7d2ffb8f01778df6ee
theluposki <theluposki@gmail.com>
2023-07-02 14:05:20 -0300

    lintstaged

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { Express } from 'express'
-import cors from 'cors'
-import config from './config.js'
-import cookieParser from 'cookie-parser';
-import routes from './routes/index.ts'
-import { errorHandlerJSON } from './middlewares/JSONFormat.ts';
-const app:Express = express()
+import express, { Express } from "express";
+import cors from "cors";
+import config from "./config.js";
+import cookieParser from "cookie-parser";
+import routes from "./routes/index.ts";
+import { errorHandlerJSON } from "./middlewares/JSONFormat.ts";
+const app: Express = express();
 
-app.use(cors(config.cors))
+app.use(cors(config.cors));
 app.use(cookieParser());
-app.use(express.json())
-app.use(errorHandlerJSON)
-app.use('/', express.static('src/public'))
+app.use(express.json());
+app.use(errorHandlerJSON);
+app.use("/", express.static("src/public"));
 
-routes(app)
+routes(app);
 
-export default app
+export default app;
